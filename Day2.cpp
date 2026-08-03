ee66efbd02546ea99107299a1b0cef365920c597
SHARVAN223 <sharvan70458@gmail.com>
2026-08-04 02:52:52 +0530

    resursion 3

--- Day2.cpp
+++ Day2.cpp
@@ -2,17 +2,60 @@
 #include <vector>
 using namespace std;
 
+// Q. Check if sorted array
 
-bool isSorted(vector <int> arr , int n){
-    if (n==0 || n==1){
-        return true;
+// bool isSorted(vector <int> arr , int n){
+//     if (n==0 || n==1){
+//         return true;
+//     }
+//     return arr[n-1] >= arr[n-2] && isSorted(arr , n-1);
+// }
+
+
+// int main(){
+//     vector <int> arr = {1,2,3,4,5};
+//     cout<<isSorted(arr, arr.size());
+//     return 0;
+// }
+
+
+// Q.Binary Search
+
+int Bs(int arr[], int target, int st, int end)
+{
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+           return Bs(arr , target, mid+1 , end);
+        }
+        else
+        {
+            return Bs(arr, target , st , mid-1);
+        }
     }
-    return arr[n-1] >= arr[n-2] && isSorted(arr , n-1);
+
+    return -1;
 }
 
+int main()
+{
+    int arr[] = {-1, 0, 3, 5, 9, 12};
 
-int main(){
-    vector <int> arr = {1,2,3,4,5};
-    cout<<isSorted(arr, arr.size());
+    int n = sizeof(arr);
+
+    int st = 0;
+    int end = n - 1;
+
+    int target = 5;
+
+    cout << Bs(arr, target, st, end);
+
     return 0;
 }
